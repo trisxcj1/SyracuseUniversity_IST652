39daabc9faf2b412ac68e258878599528bab9a4e
trisxcj1 <trisxcjoseph@gmail.com>
2021-07-02 09:52:27 -0400

    included the output

--- Week 4/Async4.4.py
+++ Week 4/Async4.4.py
@@ -1,3 +1,4 @@
+# Code:
 import numpy as np
 
 # Use index slicing and show how to get the subarray consisting of the last 2 rows, and 3rd and 4th columns
@@ -12,3 +13,15 @@ print(np_array[2:4, 2:4])
 col_sums_array = np.asarray(np_array.sum(axis=0)).reshape(1, 5)
 print("One dimensional array that sums the columns:")
 print(col_sums_array)
+
+# Output:
+# Full array:
+# [[ 1  2  3  4  5]
+#  [ 6  7  8  9 10]
+#  [11 12 13 14 15]
+#  [16 17 18 19 20]]
+# Subarray:
+# [[13 14]
+#  [18 19]]
+# One dimensional array that sums the columns:
+# [[34 38 42 46 50]]
